044be5ea7f4c54b25f66650063bb013e5a15e21c
Thanh138 <thanhncse172947@fpt.edu.vn>
2023-10-30 09:45:42 +0700

    PUSH

---
+++ SeleniumWebdriver_Ecommerceproject/src/main/java/org/example/Register.java
@@ -0,0 +1,78 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
+
+public class Register {
+    WebDriver driver;
+
+    By myAccountLink = By.xpath("(//a[@title='My Account'][normalize-space()='My Account'])[2]");
+    By crateAccountLink = By.xpath("//a[@title='Register']");
+    By firstnameInput = By.xpath("//input[@id='firstname']");
+    By lastnameInput = By.xpath("//input[@id='lastname']");
+    By MiddleNameInput = By.xpath("//input[@id='middlename']");
+    By emailInput = By.xpath("//input[@id='email_address']");
+    By passwordInput = By.xpath("//input[@id='password']");
+    By confirmPasswordInput = By.xpath("//input[@id='confirmation']");
+    By registerButton = By.cssSelector("[href='http://live.techpanda.org/index.php/customer/account/']");
+
+    By getRegisterButton = By.xpath("//button[@title='Register']");
+    public Register(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void clickMyAccountLink() {
+        driver.findElement(myAccountLink).click();
+    }
+
+    public void clickCreateAccountLink() {
+
+        driver.findElement(crateAccountLink).click();
+    }
+
+    public void enterFirstName(String firstName) {
+        WebElement element = driver.findElement(firstnameInput);
+        element.clear();
+        element.sendKeys(firstName);
+    }
+
+    public void enterMiddleName(String middleName) {
+        WebElement element = driver.findElement(MiddleNameInput);
+        element.clear();
+        element.sendKeys(middleName);
+    }
+
+    public void EnterLastName(String lastname) {
+        WebElement element = driver.findElement(lastnameInput);
+        element.clear();
+        element.sendKeys(lastname);
+    }
+    public void enterEmail(String email) {
+        WebElement element = driver.findElement(emailInput);
+        element.clear();
+        element.sendKeys(email);
+    }
+
+    public void enterPassword(String password) {
+        WebElement element = driver.findElement(passwordInput);
+        element.clear();
+        element.sendKeys(password);
+    }
+
+    public void enterConfirmPassword(String confirmPassword) {
+        WebElement element = driver.findElement(confirmPasswordInput);
+        element.clear();
+        element.sendKeys(confirmPassword);
+    }
+
+    public void clickRegister() {
+        driver.findElement(registerButton).click();
+    }
+
+    public void getRegisterButton() {
+        driver.findElement(getRegisterButton).click();
+    }
+}
